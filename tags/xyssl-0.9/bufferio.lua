06ff88090161b06359a724f0d2f501ed58fb3b8b
garyng2000 <garyng2000@dffa0898-e435-0410-b2a1-f1d76ac342e6>
2008-03-30 13:26:44 +0000

    tags the xyssl-0.9 version

---
+++ tags/xyssl-0.9/bufferio.lua
@@ -0,0 +1,144 @@
+--[[
+--
+a buffer io module to simulate luasocket's io function
+on top of raw non-buffered raw io objects(pipe/ssl etc.)
+
+Copyright 2007 Gary Ng<linux@garyng.com>
+This code can be distributed under the LGPL license
+
+--]]
+local meta={};meta.__index = meta
+local type=type
+local getmetatable=getmetatable
+local setmetatable=setmetatable
+local pairs=pairs
+local print=print
+local time=os.time
+local tjoin=table.concat
+local string=string
+
+module'bufferio'
+
+local CHUNK_SIZE = 8192
+
+
+local function dirty(self)
+    --local x,err,c = self:receive(0)
+    return (self._last and #self._last > 0) or self.p:dirty() or self.closed
+end
+
+local function receive(self, pattern, part)
+    local underlying_read = self.p.read or self.p.receive
+
+    if self.buffered_read then
+       local x,msg,chunk = self.p:receive(pattern,part) 
+       if msg and msg ~= "timeout" then self.closed = true end 
+       return x,msg,chunk
+    end
+    pattern = pattern or "*l"
+    part = (part or "") .. (self._last or "") --all partial
+    self._last = ""
+    local size = #part
+    if type(pattern) == "number" then
+        local data, err, chunk
+        if pattern == 0 or pattern > size then
+            data,err,chunk = underlying_read(self.p, pattern == 0 and 256 or pattern - size)
+            data = part .. (data or chunk or "")
+            size = #data
+            if pattern == 0 then self._last = data end
+        else
+            data = part:sub(1, pattern) 
+            self._last = part:sub(pattern + 1)
+            size = pattern
+        end
+        if err and err ~= "timeout" then self.closed = true end
+        if size < pattern then 
+            return nil, err, data
+        elseif pattern == 0 and err then
+            return nil, err, ""
+        else return data end
+    elseif pattern == "*a" or pattern == "*all" then
+        local t={part}
+        local data=""
+        local data,err,chunk 
+        repeat 
+            t[#t+1] = data
+            data,err,chunk = underlying_read(self.p, CHUNK_SIZE)
+            if err and err ~= "timeout" then self.closed = true end
+        until not data or #data == 0
+        return tjoin(t,""), err
+    elseif pattern == "*l" then
+        self._last = part
+        local nl,data,err,chunk
+        nl = self._last:find("\n")
+        while not nl do
+            data, err, chunk = underlying_read(self.p, 64)
+            self._last = self._last .. (data or chunk or "")
+            nl = self._last:find("\n")
+            if not data then break end
+        end
+        if nl then
+            data = self._last:sub(1, nl - 1)
+            self._last = self._last:sub(nl + 1)
+            if data:sub(-1) == '\r' then return data:sub(1,-2)
+            else return data end
+        else
+            data = self._last
+            self._last = ""
+            if err and err ~= "timeout" then self.closed = true end
+            if err=="timeout" then return nil, err, data or ""
+            else return nil, err or "closed", data end
+        end
+    end
+end
+
+local function send(self, data, i, j)
+    if #data == 0 then
+        local x,err,c = receive(self, 0)
+        if err and err ~= "timeout" then 
+            self.closed = true
+            return nil,err,0 
+        end
+    end
+
+    if self.buffered_write then
+       local x,err,c =  self.p:send(data,i,j) 
+       if err and err ~= "timeout" then self.closed = true end
+       return x,err,c
+    end
+   
+    i = i or 1
+    j = j or #data
+
+    if i > 1 then data = data:sub(i,j) end
+    if #data == 0 then return i - 1 end
+    
+    local written,msg,e = self.p:send(data)
+
+    if written then
+        return i + written - 1
+    else
+        if msg and msg ~= "timeout" then self.closed = true end 
+        return nil, msg, i + e - 1
+    end
+end
+
+function wrap(p, buffered_write,buffered_read)
+    if buffered_write == nil then buffered_write = p.buffered_write end
+    local o = {p=p, buffered_write=buffered_write,buffered_read=buffered_read}
+    for k,v in pairs(getmetatable(p).__index) do
+        if type(v) == "function" then
+            o[k] = function(self,...)
+                local f = v
+                return v(self.p,...)
+            end
+        end
+    end
+    o.receive = receive
+    o.read = receive
+    o.send = send
+    o.write = send
+    o.dirty = dirty
+    
+    return o
+end
---
+++ tags/xyssl-0.9/httpd-co.lua
@@ -0,0 +1,210 @@
+require'socket'
+require'lxyssl'
+require'bufferio'
+require 'copas' 
+format = string.format
+
+SESSION_LIVE = 2400
+SESSION_ROUNDS = 1000
+MAX_SESSIONS = 10000
+MAX_SSL = 100
+--copas.WATCH_DOG_TIMEOUT = 600
+
+string.hex = function(x)
+    local t={}
+    for c in x:gmatch('(.)') do t[#t+1]=string.format("%02x", c:byte()) end
+    return table.concat(t,"")
+end
+
+local function proto_index(o, k)  
+    --local v = o.__proto[k]
+    local v = rawget(o, '__proto')[k]
+    if type(v) == "function" then return function(x,...) return v(o.__proto,...) end 
+    else return v end
+end
+
+local function prototype(o)
+    return setmetatable({__proto=o}, {__index = proto_index })
+end
+
+local function header(l)
+    local k,v = l:match("^%s*([^:]*):(.*)$")
+    return k:match("^%s*(.-)%s*$"), v:match("^%s*(.-)%s*$")
+end
+
+local function keep_alive(proto, c)
+    if proto:find('1.0') and c == 'keep-alive' then
+        return "Connection: Keep-Alive\r\n"
+    end
+end
+
+local function H501(obj,verb, h,uri,proto,read,write)
+    local data
+    local alive = keep_alive(proto, h['connection'])
+    write(format("%s 501 No Implemented\r\n", proto))
+    if alive then write(alive) end
+    write("Content-Type: plain/text\r\n")
+    local r  = format("%s %s\r\n", verb, uri)
+    write(format("Content-Length: %i\r\n", #r))
+    write("\r\n")
+    write(r)
+end
+
+local function H404(obj,verb, h,uri,proto,read,write)
+    local data
+    local alive = keep_alive(proto, h['connection'])
+    write(format("%s 404 Not Found\r\n", proto))
+    if alive then write(alive) end
+    write("Content-Type: plain/text\r\n")
+    local r  = format("%s %s\r\n", verb, uri)
+    write(format("Content-Length: %i\r\n", #r))
+    write("\r\n")
+    write(r)
+end
+
+local dispatch={
+    ['GET'] = function(obj,verb, h,uri,proto,read,write)
+        local data= format("%s %s %s\r\n", verb, uri, ("a"):rep(3))
+        local alive = keep_alive(proto, h['connection'])
+        write(format("%s 200 OK\r\n",proto))
+        local too_old = os.difftime(os.time(), obj.birthday) >= SESSION_LIVE or obj.freq >= SESSION_ROUNDS 
+        if alive and not too_old then write(alive) end
+        if too_old and not proto:find('1.0') then write("Connection: close\r\n") end
+        write("Content-Type: plain/text\r\n")
+        write(format("Content-Length: %i\r\n", #data))
+        write("\r\n")
+        write(data)
+    end,
+    ['PUT'] = function(obj,verb, h,uri,proto,read,write)
+        local data
+        local alive = keep_alive(proto, h['connection'])
+        if h['content-length'] then
+           data=b:receive(h['content-length'])
+        elseif h['transfer-encoding'] and h['transfer-encoding']:find('chunked') then
+            local cnt
+            local c={}
+            repeat
+                cnt,err,chunk = read()
+                if cnt and #cnt > 0 then
+                   c[#c+1]=read(cnt) 
+                end
+                read()
+            until cnt == 0
+            data = table.concat(c,"")
+        else
+            
+        end
+        write(format("%s 200 OK\r\n", proto))
+        local too_old = os.difftime(os.time(), obj.birthday) > SESSION_LIVE or obj.freq < SESSION_ROUNDS 
+        if alive or not proto:find('1.0') then
+            if too_old then write("Connection: close\r\n")
+            elseif alive then write(alive) end
+        end
+        write("Content-Type: plain/text\r\n")
+        local r  = format("%s %s size %i\r\n", verb, uri, #data)
+        write(format("Content-Length: %i\r\n", #r))
+        write("\r\n")
+        write(r)
+    end,
+}
+
+local sessions=setmetatable({},{__mode="kv"})
+local function handler(skt,is_ssl)
+    skt:setoption('tcp-nodelay', true)
+    local ip,port = skt:getsockname()
+
+    local x = lxyssl.ssl(1) --1 is ssl server nil or 0 is client
+    getmetatable(x).get_session = function(o, id, cipher)
+    local s = sessions[id]
+    if s and s.cipher == cipher then return s.master end
+    end
+    getmetatable(x).set_session = function(o, id, cipher, master)
+    sessions[id] = {cipher=cipher, master=master}
+    end
+
+    --local b = bufferio.wrap(port == 4433 and x or skt, true, port ~= 4433)
+    local b = is_ssl and bufferio.wrap(x) or prototype(skt)
+
+    if not port then return end
+    x:keycert() --setup server cert, would use embedded testing one none is given
+    x:connect(skt:getfd())
+    
+    --b:settimeout(-1)
+    local function read(...) return copas.receive(b,...) end 
+    local function write(...) return copas.send( b,...) end 
+    local obj = b
+    
+    obj.birthday = os.time()
+    obj.freq = 1
+    --obj.probe = function(skt) return lxyssl.probe(skt:getfd()) end
+    --local client = (port == 4433) and copas.wrap(b) or copas.wrap(skt)
+    --local client = copas.wrap(skt)
+    local action,err,chunk = read()
+    while action and #action > 0 do
+        local h = {}
+        local data
+        local verb, resource, proto = action:match("^%s*([^%s]*)%s+([^%s]*)%s+(.*)$")
+        if not verb then break end
+        repeat
+            local l,err,chunk = read()
+            if l and #l > 0 then
+                local k,v = header(l)
+                h[k:lower()]=v:lower()
+            end
+        until not l or #l==0
+        local f = dispatch[verb:upper()]
+        if not f then f = dispatch['H501'] end
+        if f then 
+            f(obj,verb, h, resource, proto, read, write) 
+        else
+            H501(obj,verb, h, resource, proto, read, write) 
+        end
+        if (not proto:find("1.0") or h['connection'] == 'keep-alive') 
+            and os.difftime(os.time(), obj.birthday) < SESSION_LIVE 
+            and obj.freq < SESSION_ROUNDS then 
+            action, err, chunk = read()
+            obj.freq = obj.freq + 1
+        else action = nil end
+    end
+    obj:close() 
+    --x:close()
+end
+
+local e
+local ssl_connections = 0
+local connections = 0
+local function server(p,ssl)
+    local function http_handler(skt)
+        if ssl_connections > MAX_SESSIONS then return end
+        connections = connections + 1 
+        local x =  {handler(skt)}
+        connections = connections - 1 
+        return unpack(x)
+    end
+    local function https_handler(skt)
+        if ssl_connections > MAX_SSL then return end
+        ssl_connections = ssl_connections + 1
+        local x =  {handler(skt,1)}
+        ssl_connections = ssl_connections - 1
+        return unpack(x)
+    end
+
+    local tcp = socket.bind("*",tonumber(p))
+    if ssl then copas.addserver(tcp, https_handler)
+    else copas.addserver(tcp, http_handler) end
+end
+
+server(4433,1)
+server(8080)
+while true do
+    copas.step(0.1)
+    --print(connections)
+    --local x = tcp:accept()
+    --if x then proxy_handler(x) end
+end
+--[[
+t = socket.bind('localhost',4433)
+while true do
+s = t:accept()
+handler(s)
+end]]
